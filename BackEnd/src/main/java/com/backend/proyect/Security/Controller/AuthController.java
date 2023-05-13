6e0c4262b5a08d35ad893304240064fc5fb4fd3f
Jeremias Bozal <miliciametal87@gmail.com>
2023-05-13 20:51:58 -0300

    cambios en cross origin
    cambios en cross origin

--- BackEnd/src/main/java/com/backend/proyect/Security/Controller/AuthController.java
+++ BackEnd/src/main/java/com/backend/proyect/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://porfolioarg.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
@@ -43,6 +43,9 @@ public class AuthController {
     RolService rolService;
     @Autowired
     JwtProvider jwtProvider;
+    
+    
+    
     
     @PostMapping("/nuevo")
     public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
